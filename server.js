8d898186694d801da5bfae978df670702adbd43d
James Kanka <james.kanka@gmail.com>
2015-11-18 20:29:24 -0600

    Add delete todos route

--- server.js
+++ server.js
@@ -44,6 +44,19 @@ app.post('/todos', function (req, res) {
 	
 });
 
+// DELETE /todos/:id
+app.delete('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	
+	if (!matchedTodo) {
+		res.status(404).json({"Error": "No todo found with that ID"});
+	} else{
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
+});
+
 app.listen(PORT, function () {
 	console.log('Express listening to port' + PORT + '!');
 });
