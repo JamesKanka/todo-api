eb955d2e2e3e217cc0fa6df24ed7e17d5d84bc0c
James Kanka <james.kanka@gmail.com>
2015-11-18 20:06:43 -0600

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -19,18 +21,7 @@ app.get('/todos', function (req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	// James' method
-	// if (todoId <= 0 || todoId > todos.length ) {
-	// 	res.status(404).send();
-	// } else {
-	// 	res.json(todos[todoId - 1]);
-	// }
-	var matchedTodo;
-	todos.forEach(function (todo) {
-		if (todoId === todo.id) {
-			matchedTodo = todo;
-		}
-	});
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 	if (matchedTodo) {
 		res.json(matchedTodo);
 	} else {
@@ -40,14 +31,17 @@ app.get('/todos/:id', function (req, res) {
 
 // POST /todos
 app.post('/todos', function (req, res) {
-	var body = req.body;
-	// add id field
-	body.id = todoNextId++;
-	// push body into array
-	todos.push(body);
+	var body = _.pick(req.body, 'description', 'completed');
 	
-	console.log(todos);
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+	
+	body.description = body.description.trim();
+	body.id = todoNextId++;
+	todos.push(body);
 	res.json(body);
+	
 });
 
 app.listen(PORT, function () {
