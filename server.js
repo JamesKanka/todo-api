9d6d25089f7423a1cb98a03eab51de0e17524d51
James Kanka <james.kanka@gmail.com>
2015-11-20 14:39:10 -0600

    reconfigured PUT /todo/:id for sequlize

--- server.js
+++ server.js
@@ -56,7 +56,6 @@ app.get('/todos/:id', function(req, res) {
 // POST /todos
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
-
 	body.description = body.description.trim();
 	db.todo.create(body).then(function(todo) {
 		res.json(todo.toJSON());
@@ -87,30 +86,27 @@ app.delete('/todos/:id', function(req, res) {
 app.put('/todos/:id', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
+	var attributes = {};
+
+	if (body.hasOwnProperty('completed')) {
+		attributes.completed = body.completed;
+	} 
+	if (body.hasOwnProperty('description')) {
+		attributes.description = body.description;
+	} 
+	db.todo.findById(todoId).then(function (todo){
+		if (todo) {
+			todo.update(attributes).then(function (todo) {
+				res.json(todo.toJSON());
+			}, function(e) {
+				res.status(400).json(e);
+			});
+		} else {
+			res.status(404).send();
+		}
+	}, function() {
+		res.status(500).send();
 	});
-	var validAttributes = {};
-
-	if (!matchedTodo) {
-		return res.status(404).send();
-	}
-
-	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-		validAttributes.completed = body.completed;
-	} else if (body.hasOwnProperty('completed')) {
-		return res.status(400).send();
-	}
-
-	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-		validAttributes.description = body.description;
-	} else if (body.hasOwnProperty('description')) {
-		return res.status(400).send();
-	}
-
-	// HERE
-	_.extend(matchedTodo, validAttributes);
-	res.json(matchedTodo);
 });
 
 db.sequelize.sync().then(function() {
