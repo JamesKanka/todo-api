68498a6e01d7d4873fc7c75d280d23d756897c90
James Kanka <james.kanka@gmail.com>
2015-11-21 21:54:07 -0600

    Authorize requet by user

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ app.get('/', function(req, res) {
 // GET /todos?completed=true&q=work
 app.get('/todos', middleware.requireAuthentication, function(req, res) {
 	var query = req.query;
-	var where = {};
+	var where = {
+		userId: req.user.get('id')
+	}; 
 
 	if (query.hasOwnProperty('completed') && query.completed === 'true') {
 		where.completed = true;
@@ -32,7 +34,7 @@ app.get('/todos', middleware.requireAuthentication, function(req, res) {
 			$like: '%' + query.q + '%'
 		};
 	}
-
+console.log(where);
 	db.todo.findAll({
 		where: where
 	}).then(function(todos) {
@@ -46,7 +48,12 @@ app.get('/todos', middleware.requireAuthentication, function(req, res) {
 app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	
-	db.todo.findById(todoId).then(function(matchedTodo) {
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+		}
+	}).then(function(matchedTodo) {
 		if (!matchedTodo) {
 			return res.status(404).send();
 		}
@@ -74,7 +81,13 @@ app.post('/todos', middleware.requireAuthentication, function(req, res) {
 // DELETE /todos/:id
 app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	db.todo.findById(todoId).then(function(todo) {
+	
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+		}
+	}).then(function(todo) {
 		if (todo) {
 			todo.destroy().then(function() {
 				res.json(todo);
@@ -101,7 +114,12 @@ app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	if (body.hasOwnProperty('description')) {
 		attributes.description = body.description;
 	}
-	db.todo.findById(todoId).then(function(todo) {
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+		}
+	}).then(function(todo) {
 		if (todo) {
 			todo.update(attributes).then(function(todo) {
 				res.json(todo.toJSON());
@@ -145,7 +163,7 @@ app.post('/users/login', function(req, res) {
 
 
 
-db.sequelize.sync({force: true}).then(function() {
+db.sequelize.sync().then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening to port ' + PORT + '!');
 	});
